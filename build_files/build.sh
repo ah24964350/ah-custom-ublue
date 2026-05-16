0fa10edc25e8acec52f4c233df93c199e48abd09
adam <adamhouston78@gmail.com>
2026-05-16 21:42:21 +0100

    trying again to get Grok to correct its own mistakes, and build ly into the image.

--- build_files/build.sh
+++ build_files/build.sh
@@ -179,3 +179,54 @@ systemctl enable ly@tty2.service
 ### Final cleanup
 dnf5 clean all
 rm -rf /var/cache/* /tmp/* /var/tmp/*
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
+dnf5 install -y adw-gtk3-theme cmatrix fastfetch geany gtkhash-thunar \
+    qt5-qtwayland qt5ct qt6-qtwayland qt6-qtwayland-adwaita-decoration qt6ct \
+    sway-config-fedora Thunar-docs thunar-media-tags-plugin thunar-vcs-plugin thunar-volman vlc 
+
+# install groups
+dnf5 install -y --setopt=install_weak_deps=0 @swaywm-extended @virtualization
+
+# Wezterm nightly COPR
+dnf5 -y copr enable wezfurlong/wezterm-nightly
+dnf5 install -y wezterm
+
+### === LY DISPLAY MANAGER (built from source) ===
+# Build dependencies (official from ly README)
+dnf5 install -y --setopt=install_weak_deps=0 \
+    zig \
+    pam-devel \
+    libxcb-devel
+
+# Runtime dependencies needed by ly
+dnf5 install -y --setopt=install_weak_deps=0 \
+    xorg-x11-xauth \
+    xorg-x11-server \
+    brightnessctl
+
+# Clone, build and install ly (official systemd method)
+git clone --depth 1 https://codeberg.org/fairyglade/ly.git /tmp/ly
+cd /tmp/ly
+zig build installexe -Dinit_system=systemd
+cd /
+rm -rf /tmp/ly
+
+# Make ly the default on tty2
+systemctl disable gdm.service sddm.service lightdm.service || true
+systemctl disable getty@tty2.service || true
+systemctl enable ly@tty2.service
+
+### Final cleanup
+dnf5 clean all
+rm -rf /var/cache/* /tmp/* /var/tmp/*
